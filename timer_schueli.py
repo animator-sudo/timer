9c8cf605d4c1bde0472b2ef15361829ba1f453ff
animator-sudo <mario.imhof@gmail.com>
2025-06-22 11:25:20 +0200

    Update timer_schueli.py

--- timer_schueli.py
+++ timer_schueli.py
@@ -15,44 +15,13 @@ with open("ilgen_lions.png", "rb") as f:
     encoded = f.read()
 b64 = base64.b64encode(encoded).decode()
 
-# CSS für responsives Hintergrundbild
+# CSS mit dunklem Overlay und besserer Lesbarkeit
 st.markdown(
     f"""
     <style>
     .stApp {{
-        background-image: url("data:image/png;base64,{b64}");
+        background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
+                          url("data:image/png;base64,{b64}");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
-        background-attachment: fixed;
-    }}
-    @media only screen and (max-width: 768px) {{
-        .stApp {{
-            background-size: contain;
-            background-position: top center;
-            background-attachment: scroll;
-        }}
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Alphabetisch sortierte Kindernamen
-kinder_namen = [
-    "Annabelle", "Charlotte", "Elena", "Ella", "Filippa",
-    "Ida", "Luisa", "Meliah", "Noemi", "Uliana"
-]
-kinder_namen.sort()
-
-# Timer-Initialisierung
-if "timers" not in st.session_state:
-    st.session_state.timers = [
-        {"name": name, "start_time": None, "elapsed": 0.0, "running": False}
-        for name in kinder_namen
-    ]
-
-# Funktion zur Formatierung der Zeit
-def format_time(seconds):
-    minutes = int(seconds // 60)
-    sec = int(seconds % 60)
