946b68bead86dd0926e14ef0ae838eb195012bc6
animator-sudo <mario.imhof@gmail.com>
2025-06-22 11:33:05 +0200

    Update timer_schueli.py

--- timer_schueli.py
+++ timer_schueli.py
@@ -26,36 +26,4 @@ st.markdown(
         background-position: center;
         background-repeat: no-repeat;
         background-attachment: fixed;
-        color: white;
-    }}
-    @media only screen and (max-width: 768px) {{
-        .stApp {{
-            background-size: contain;
-            background-position: top center;
-            background-attachment: scroll;
-        }}
-    }}
-    h1, h2, h3, h4, h5, h6, .stMetric, .stButton, .stMarkdown {{
-        color: white !important;
-    }}
-    .timer-box {{
-        background-color: rgba(0, 0, 0, 0.5);
-        padding: 1em;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0,0,0,0.3);
-        margin-bottom: 10px;
-    }}
-    </style>
-    ''',
-    unsafe_allow_html=True
-)
 
-# Kindernamen nach Anfangsbuchstaben sortieren
-kinder_namen = sorted([
-    "Annabelle", "Charlotte", "Elena", "Ella", "Filippa",
-    "Ida", "Luisa", "Meliah", "Noemi", "Uliana"
-], key=lambda name: (name[0], name))
-
-# Timer-Initialisierung
-if "timers" not in st.session_state:
-    st.session_state.timers = 
